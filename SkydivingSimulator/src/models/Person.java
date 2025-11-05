c5bbc99b58a4b8ccaf7e82ae57e03af1f07d1b2e
Usman M <musman2007@outlook.com>
2025-11-05 18:25:34 -0500

    Organized the classes into their own respective packages and started the Person class

---
+++ SkydivingSimulator/src/models/Person.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Usman
+ */
+public class Person {
+    private int height; // The height of the person
+    private int weight; // The weight of the person
+    private int projectedArea; // Surface area of the side that is facing air flow
+    
+    public Person() {
+        
+    }
+}
