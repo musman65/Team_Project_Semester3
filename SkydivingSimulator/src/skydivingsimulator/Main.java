952293ab3d445ad5db2891c500bf28ee5182b9fe
Usman Muhammad <>
2025-11-05 17:47:13 -0500

    Test commit & push from NetBeans

--- SkydivingSimulator/src/skydivingsimulator/Main.java
+++ SkydivingSimulator/src/skydivingsimulator/Main.java
@@ -2,24 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package javafx;
+package skydivingsimulator;
 
 import java.io.IOException;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
+/**Stage
  *
  * @author sahel
  */
-public class Project extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
-        Application.launch(Project.class, args);
+        Application.launch(Main.class, args);
     }
 
     @Override
