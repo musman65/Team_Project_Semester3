37fee1bfc77672bd8a5d13b4fb406c358b54fa9c
Usman M <musman2007@outlook.com>
2025-11-19 22:59:29 -0500

    Reverted changes, added .gitignore

---
+++ SkydivingSimulator/src/models/MainLogic.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Usman
+ */
+public class MainLogic {
+    
+    
+    public static Double[] eulerMethod(Double[] previous ) {
+        Double[] next = new Double[previous.length];
+        
+        return next;
+    }
+    
+    public static Double acceleration() {
+        return 0.0;
+    }
+    
+    public static Double gravitationalForce(double mass) {
+        return (Double) mass * 9.8;
+    }
+    
+//    public static Double dragForce()
+}
