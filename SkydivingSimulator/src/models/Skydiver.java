18d3b0c5b85e6ea8c3100f85b7151dc829f49019
Aswinth Sinnathamby <aswinthsinnathamby@gmail.com>
2025-11-26 15:50:01 -0500

    Did constructor

--- SkydivingSimulator/src/models/Skydiver.java
+++ SkydivingSimulator/src/models/Skydiver.java
@@ -10,34 +10,46 @@ TODO BY: Aswinth
 Represents the physical state of the skydiver,
 storing dynamic variables like current height,
 velocity, acceleration, and methods to update them.
-*/
-
+ */
 /**
  *
  * @author Usman
  */
 public class Skydiver {
-    // Methods only made to remove errors from code, they will written by Aswinth 
+
+    private SimulationParameters Params = new SimulationParameters();
+    private double CurrentPosition;
+    private double CurrentVelocity;
+    private double CurrentAcceleration;
+    private double CurrentNetForce;
     
-    public SimulationParameters getParams() {
-        return null;
+    public void Skydiver(double CurrentPosition, double CurrentVelocity, double CurrentAcceleration, double CurrentNetForce, SimulationParameters Params) {
+        this.CurrentPosition = CurrentPosition;
+        this.CurrentAcceleration = CurrentAcceleration;
+        this.CurrentNetForce = CurrentNetForce;
+        this.CurrentVelocity = CurrentVelocity;
+        this.Params = Params;
+        
     }
-    
+
+    public SimulationParameters getParams() {
+        return Params;
+    }
+
     public double getCurrentPosition() {
         return 0.0;
     }
-    
+
     public double getCurrentVelocity() {
         return 0.0;
     }
-    
+
     public double getCurrentAcceleration() {
         return 0.0;
     }
-    
+
     public double getCurrentNetForce() {
         return 0.0;
     }
-    
-    
+
 }
