25c90ce78b5b03b574c0c61abc024a713e6d2352
Usman M <musman2007@outlook.com>
2025-11-28 17:08:48 -0500

    Made the graphs interface and pulled the MainView from main

--- SkydivingSimulator/src/models/SimulationResults.java
+++ SkydivingSimulator/src/models/SimulationResults.java
@@ -14,5 +14,37 @@ TODO BY: Usman
  * @author Usman
  */
 public class SimulationResults {
-    
+    private double time;
+    private double position;
+    private double velocity;
+    private double acceleration;
+    private double force;
+
+    public SimulationResults(double time, double position, double velocity, double acceleration, double force) {
+        this.time = time;
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+        this.force = force;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public double getPosition() {
+        return position;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public double getAcceleration() {
+        return acceleration;
+    }
+
+    public double getForce() {
+        return force;
+    }
 }
