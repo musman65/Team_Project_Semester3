c27dfea8b4becdedce391636835ac2ff4b1e2aed
sahel <sahel@DESKTOP-DCP2E5A>
2025-11-29 13:06:13 -0500

    Made some changes

--- SkydivingSimulator/src/controllers/StartScreenController.java
+++ SkydivingSimulator/src/controllers/StartScreenController.java
@@ -4,11 +4,18 @@
  */
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,16 +30,20 @@ public class StartScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
-    private void enter(ActionEvent event) {
+    private void enter(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));
+        stage.getScene().setRoot(root);
+       
     }
 
     @FXML
     private void exit(ActionEvent event) {
-        
-        
+        Platform.exit();
+
     }
-    
+
 }
